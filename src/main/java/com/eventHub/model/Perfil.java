9a550bd61c477f3f9ac5ca2da361064817fac7b5
BrunoMano2004 <brunomano2004@gmail.com>
2024-06-13 15:32:42 -0300

    Criando a model usuario

---
+++ src/main/java/com/eventHub/model/Perfil.java
@@ -0,0 +1,7 @@
+package com.eventHub.model;
+
+public enum Perfil {
+    ORGANIZADOR,
+    PARTICIPANTE,
+    ADMINISTRADOR
+}
---
+++ src/main/java/com/eventHub/model/Usuario.java
@@ -0,0 +1,36 @@
+package com.eventHub.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String telefone;
+
+    private Perfil perfil;
+
+    private Timestamp dataCriacao;
+}
