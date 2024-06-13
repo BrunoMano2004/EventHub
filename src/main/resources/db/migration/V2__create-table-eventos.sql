809feb3864c7edad506e797e0dfec2e4f24b53aa
BrunoMano2004 <brunomano2004@gmail.com>
2024-06-13 15:19:19 -0300

    Criando tabela eventos pelo flyway

---
+++ src/main/resources/db/migration/V2__create-table-eventos.sql
@@ -0,0 +1,14 @@
+create table eventos(
+    id bigint primary key auto_increment,
+    organizador_id BIGINT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    data_inicio TIMESTAMP NOT NULL,
+    data_fim TIMESTAMP NOT NULL,
+    local VARCHAR(255) NOT NULL,
+    capacidade INT NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    status ENUM('ativo', 'cancelado') DEFAULT 'ativo',
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (organizador_id) REFERENCES usuarios(id)
+)
